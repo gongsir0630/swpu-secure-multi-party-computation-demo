92271a72f5423a224c15eefffc962c643a355bec
zwj <958654064@qq.com>
2020-04-03 10:47:50 +0800

    政府端细节的修改

--- dubbo-test-consumer/src/main/java/cn/edu/swpu/wlzx/consumer/controller/GovController.java
+++ dubbo-test-consumer/src/main/java/cn/edu/swpu/wlzx/consumer/controller/GovController.java
@@ -47,16 +47,16 @@ public class GovController {
     private static final Logger logger = LoggerFactory.getLogger(GovController.class);
 
     @Reference(version = "1.0.0",group = "govA")
-    GovUserService govAUserService;
+    private GovUserService govAUserService;
 
     @Reference(version = "1.0.0",group = "govB")
-    GovUserService govBUserService;
+    private GovUserService govBUserService;
 
     @Reference(version = "1.0.0",group = "govA")
-    DataService dataAService;
+    private DataService dataAService;
 
     @Reference(version = "1.0.0",group = "govB")
-    DataService dataBService;
+    private DataService dataBService;
 
     /**
      *  查询所有用户审批
@@ -79,128 +79,146 @@ public class GovController {
             logger.info("=====>查询失败");
             return ResponseEntity.ok(new Result(101,"查询失败"));
         }
-        Map<String,Object> map=new HashMap<>(20);
-        map.put("userA",userList1);
-        map.put("userB",userList2);
         Result result=new Result(100,"查询成功");
         logger.info("=====>查询成功");
-        result.setData(map);
+        result.putData("userA",userList1);
+        result.putData("userB",userList2);
         return ResponseEntity.ok(result);
     }
 
     /**
-     * 审核用户的对数据源A的申请
+     * 审核用户的对数据源A,B的申请
      * @return 审核结果
      */
     @ApiOperation(value = "审核申请",notes = "审核用户的对数据A的申请")
     @ApiImplicitParams({
+            @ApiImplicitParam(name = "gov",value = "政府源区分"),
             @ApiImplicitParam(name = "id",value = "用户id")
     })
     @ApiResponses({
             @ApiResponse(code = 100,message = "审核成功"),
             @ApiResponse(code = 101,message = "审核失败")
     })
-    @RequestMapping(value = "/a/checkApply/{id}",method = RequestMethod.PUT)
-    public ResponseEntity<Result> reviewApplyA(@PathVariable("id") int id){
-        if(govAUserService.updateApply(id)){
-            logger.info("=====>数据A审核成功");
-            return ResponseEntity.ok(new Result(100,"审核成功"));
+    @RequestMapping(value = "/{gov}/checkApply/{id}",method = RequestMethod.PUT)
+    public ResponseEntity<Result> reviewApplyA(@PathVariable("gov") String gov ,@PathVariable("id") int id){
+        String govA="a",govB="b";
+        if (gov.equals(govA)){
+            if(govAUserService.updateApply(id)){
+                logger.info("=====>数据A审核成功");
+                return ResponseEntity.ok(new Result(100,"审核成功"));
+            }
+        }
+        if (gov.equals(govB)){
+            if(govBUserService.updateApply(id)){
+                logger.info("=====>数据B审核成功");
+                return ResponseEntity.ok(new Result(100,"审核成功"));
+            }
         }
         return ResponseEntity.ok(new Result(101,"审核失败"));
     }
 
     /**
-     * 审核用户的对数据源B的申请
-     * @return 审核结果
-     */
-    @ApiOperation(value = "审核申请",notes = "审核用户的对数据源B的申请")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "id",value = "用户id")
-    })
-    @ApiResponses({
-            @ApiResponse(code = 100,message = "审核成功"),
-            @ApiResponse(code = 101,message = "审核失败")
-    })
-    @RequestMapping(value = "/b/checkApply/{id}",method = RequestMethod.PUT)
-    public ResponseEntity<Result> reviewApplyB(@PathVariable("id") int id){
-        if(govBUserService.updateApply(id)){
-            logger.info("=====>数据B审核成功");
-            return ResponseEntity.ok(new Result(100,"审核成功"));
-        }
-        return ResponseEntity.ok(new Result(101,"审核失败"));
-    }
-
-    /**
-     * 增加数据A
+     * 增加数据A,B
      * @return 插入结果
      */
-    @ApiOperation(value = "增加数据",notes = "在数据源A增加数据")
+    @ApiOperation(value = "增加数据",notes = "在数据源A,B增加数据")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "dataA",value = "数据A")
+            @ApiImplicitParam(name = "gov",value = "数据源区分"),
+            @ApiImplicitParam(name = "data",value = "数据")
     })
     @ApiResponses({
             @ApiResponse(code = 100,message = "插入成功"),
             @ApiResponse(code = 101,message = "插入失败")
     })
-    @PostMapping("/a/insert")
-    public ResponseEntity<Result> insertDataA(@RequestParam("dataA") String dataA){
-        if (dataAService.insertData(dataA)){
-            logger.info("=====>数据A插入成功");
-            return ResponseEntity.ok(new Result(100,"插入成功"));
+    @PostMapping("/{gov}/insert")
+    public ResponseEntity<Result> insertDataA(@PathVariable("gov") String gov,@RequestParam("data") String data){
+        String govA="a",govB="b";
+        if (gov.equals(govA)){
+            if (dataAService.insertData(data)){
+                logger.info("=====>数据A插入成功");
+                return ResponseEntity.ok(new Result(100,"插入成功"));
+            }
+        }
+        if (gov.equals(govB)){
+            if (dataBService.insertData(data)){
+                logger.info("=====>数据B插入成功");
+                return ResponseEntity.ok(new Result(100,"插入成功"));
+            }
         }
         return ResponseEntity.ok(new Result(101,"插入失败"));
     }
 
     /**
-     * 删除数据A
+     * 删除数据A,B
      * @param id
      * @return 删除结果
      */
-    @ApiOperation(value = "删除数据",notes = "在数据源A删除数据")
+    @ApiOperation(value = "删除数据",notes = "在数据源A,B删除数据")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "id",value = "数据A主键id")
+            @ApiImplicitParam(name = "gov",value = "数据源区分"),
+            @ApiImplicitParam(name = "id",value = "数据主键id")
     })
     @ApiResponses({
             @ApiResponse(code = 100,message = "删除成功"),
             @ApiResponse(code = 101,message = "删除失败")
     })
-    @RequestMapping(value = "/a/delete",method = RequestMethod.DELETE)
-    public ResponseEntity<Result> deleteDataA(@RequestParam("id") int id){
-        if (dataAService.deleteData(id)){
-            logger.info("=====>数据A删除成功");
-            return ResponseEntity.ok(new Result(100,"删除成功"));
+    @RequestMapping(value = "/{gov}/delete",method = RequestMethod.DELETE)
+    public ResponseEntity<Result> deleteDataA(@PathVariable("gov") String gov,@RequestParam("id") int id){
+        String govA="a",govB="b";
+        if (gov.equals(govA)) {
+            if (dataAService.deleteData(id)){
+                logger.info("=====>数据A删除成功");
+                return ResponseEntity.ok(new Result(100,"删除成功"));
+            }
+        }
+        if (gov.equals(govB)) {
+            if (dataBService.deleteData(id)){
+                logger.info("=====>数据B删除成功");
+                return ResponseEntity.ok(new Result(100,"删除成功"));
+            }
         }
         return ResponseEntity.ok(new Result(101,"删除失败"));
     }
 
     /**
-     * 修改数据A
+     * 修改数据A,B
      * @param data
      * @return 修改结果
      */
-    @ApiOperation(value = "修改数据",notes = "在数据源A修改数据")
+    @ApiOperation(value = "修改数据",notes = "在数据源A,B修改数据")
     @ApiImplicitParams({
-            @ApiImplicitParam(name="data",value = "数据A实体")
+            @ApiImplicitParam(name = "gov",value = "数据源区分"),
+            @ApiImplicitParam(name = "data",value = "数据实体")
     })
     @ApiResponses({
             @ApiResponse(code = 100,message = "修改成功"),
             @ApiResponse(code = 101,message = "修改失败")
     })
-    @RequestMapping(value = "/a/update",method = RequestMethod.PUT)
-    public ResponseEntity<Result> updateDataA(Data data){
-        if (dataAService.updateData(data)){
-            logger.info("=====>数据A修改成功");
-            return ResponseEntity.ok(new Result(100,"修改成功"));
+    @RequestMapping(value = "/{gov}/update",method = RequestMethod.PUT)
+    public ResponseEntity<Result> updateDataA(@PathVariable("gov") String gov,Data data){
+        String govA="a",govB="b";
+        if (gov.equals(govA)){
+            if (dataAService.updateData(data)){
+                logger.info("=====>数据A修改成功");
+                return ResponseEntity.ok(new Result(100,"修改成功"));
+            }
+        }
+        if (gov.equals(govB)){
+            if (dataBService.updateData(data)){
+                logger.info("=====>数据B修改成功");
+                return ResponseEntity.ok(new Result(100,"修改成功"));
+            }
         }
         return ResponseEntity.ok(new Result(101,"修改失败"));
     }
 
     /**
-     * 查询所有数据A
+     * 查询所有数据A,B
      * @return 查询结果
      */
-    @ApiOperation(value = "查询数据",notes = "分页查询数据A")
+    @ApiOperation(value = "查询数据",notes = "分页查询数据A,B")
     @ApiImplicitParams({
+            @ApiImplicitParam(name = "gov",value = "数据源区分"),
             @ApiImplicitParam(name="page",value = "页数"),
             @ApiImplicitParam(name = "pageSize",value = "每页数据")
     })
@@ -208,111 +226,31 @@ public class GovController {
             @ApiResponse(code = 100,message = "查询成功"),
             @ApiResponse(code = 101,message = "查询失败")
     })
-    @RequestMapping(value = "/a/selectAll",method = RequestMethod.GET)
-    public ResponseEntity<Result> selectAllDataA(@RequestParam("page") int page,@RequestParam("pageSize") int pageSize){
-        List<Data> dataList=dataAService.selectAllData(page,pageSize);
-        if (dataList.isEmpty()){
-            logger.info("=====>查询数据A失败");
-            return ResponseEntity.ok(new Result(101,"查询失败"));
+    @RequestMapping(value = "/{gov}/selectAll",method = RequestMethod.GET)
+    public ResponseEntity<Result> selectAllDataA(@PathVariable("gov") String gov,@RequestParam("page") int page,@RequestParam("pageSize") int pageSize){
+        String govA="a",govB="b";
+        if (gov.equals(govA)){
+            List<Data> dataList=dataAService.selectAllData(page,pageSize);
+            if (dataList.isEmpty()){
+                logger.info("=====>查询数据A失败");
+                return ResponseEntity.ok(new Result(101,"查询失败"));
+            }
+            Result result=new Result(100,"查询成功");
+            result.putData("dataA",dataList);
+            logger.info("=====>查询数据A成功",JSON.toJSONString(dataList));
+            return ResponseEntity.ok(result);
         }
-        Map<String,Object> map=new HashMap<>(20);
-        map.put("dataA",dataList);
-        Result result=new Result(100,"查询成功");
-        result.setData(map);
-        logger.info("=====>查询数据A成功",JSON.toJSONString(dataList));
-        return ResponseEntity.ok(result);
-    }
-
-    /**
-     * 增加数据B
-     * @return 插入结果
-     */
-    @ApiOperation(value = "增加数据",notes = "在数据源B增加数据")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "dataB",value = "数据B")
-    })
-    @ApiResponses({
-            @ApiResponse(code = 100,message = "插入成功"),
-            @ApiResponse(code = 101,message = "插入失败")
-    })
-    @PostMapping("/b/insert")
-    public ResponseEntity<Result> insertDataB(@RequestParam("dataB") String dataB){
-        if (dataBService.insertData(dataB)){
-            logger.info("=====>数据B插入成功");
-            return ResponseEntity.ok(new Result(100,"插入成功"));
+        if (gov.equals(govB)){
+            List<Data> dataList=dataBService.selectAllData(page,pageSize);
+            if (dataList.isEmpty()){
+                logger.info("=====>查询数据B失败");
+                return ResponseEntity.ok(new Result(101,"查询失败"));
+            }
+            Result result=new Result(100,"查询成功");
+            result.putData("dataB",dataList);
+            logger.info("=====>查询数据B成功",JSON.toJSONString(dataList));
+            return ResponseEntity.ok(result);
         }
-        return ResponseEntity.ok(new Result(101,"插入失败"));
-    }
-
-    /**
-     * 删除数据B
-     * @param id
-     * @return 删除结果
-     */
-    @ApiOperation(value = "删除数据",notes = "在数据源B删除数据")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "id",value = "数据B主键id")
-    })
-    @ApiResponses({
-            @ApiResponse(code = 100,message = "删除成功"),
-            @ApiResponse(code = 101,message = "删除失败")
-    })
-    @RequestMapping(value = "/b/delete",method = RequestMethod.DELETE)
-    public ResponseEntity<Result> deleteDataB(@RequestParam("id") int id){
-        if (dataBService.deleteData(id)){
-            logger.info("=====>数据B删除成功");
-            return ResponseEntity.ok(new Result(100,"删除成功"));
-        }
-        return ResponseEntity.ok(new Result(101,"删除失败"));
-    }
-
-    /**
-     * 修改数据B
-     * @param data
-     * @return 修改结果
-     */
-    @ApiOperation(value = "修改数据",notes = "在数据源B修改数据")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name="data",value = "数据B实体")
-    })
-    @ApiResponses({
-            @ApiResponse(code = 100,message = "修改成功"),
-            @ApiResponse(code = 101,message = "修改失败")
-    })
-    @RequestMapping(value = "/b/update",method = RequestMethod.PUT)
-    public ResponseEntity<Result> updateDataB(Data data){
-        if (dataBService.updateData(data)){
-            logger.info("=====>数据B修改成功");
-            return ResponseEntity.ok(new Result(100,"修改成功"));
-        }
-        return ResponseEntity.ok(new Result(101,"修改失败"));
-    }
-
-    /**
-     * 查询所有数据B
-     * @return 查询结果
-     */
-    @ApiOperation(value = "查询数据",notes = "分页查询数据B")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name="page",value = "页数"),
-            @ApiImplicitParam(name = "pageSize",value = "每页数据")
-    })
-    @ApiResponses({
-            @ApiResponse(code = 100,message = "查询成功"),
-            @ApiResponse(code = 101,message = "查询失败")
-    })
-    @RequestMapping(value = "/b/selectAll",method = RequestMethod.GET)
-    public ResponseEntity<Result> selectAllDataB(@RequestParam("page") int page,@RequestParam("pageSize") int pageSize){
-        List<Data> dataList=dataBService.selectAllData(page,pageSize);
-        if (dataList.isEmpty()){
-            logger.info("=====>查询数据B失败");
-            return ResponseEntity.ok(new Result(101,"查询失败"));
-        }
-        Map<String,Object> map=new HashMap<>(20);
-        map.put("dataB",dataList);
-        Result result=new Result(100,"查询成功");
-        result.setData(map);
-        logger.info("=====>查询数据B成功", JSON.toJSONString(dataList));
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(new Result(101,"查询失败"));
     }
 }
